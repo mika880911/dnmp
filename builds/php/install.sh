49216763c2683dda87c1cb6ca8f37b9f5bf7140c
mika880911 <1215stps@gmail.com>
2022-04-13 14:04:24 +0800

    add : php-imagick

--- builds/php/install.sh
+++ builds/php/install.sh
@@ -2,7 +2,7 @@
 
 version=$1
 apt install -y php${version} php${version}-fpm
-apt install -y php${version}-zip php${version}-xml php${version}-mysql php${version}-sqlite3 php${version}-curl php${version}-redis php${version}-gd   
+apt install -y php${version}-zip php${version}-xml php${version}-mysql php${version}-sqlite3 php${version}-curl php${version}-redis php${version}-gd php${version}-imagick   
 
 # 這邊要先啟動，因為如果沒有先啟動過，就直接調用 restart 會失敗
 service php${version}-fpm start
