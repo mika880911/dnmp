f0a810b5b0c15b2532459eb5f789a644b2e4ffc1
mika880911 <1215stps@gmail.com>
2022-04-19 12:41:59 +0800

    feat: add question to resest volumes directory

--- buildImage.sh
+++ buildImage.sh
@@ -1,17 +1,44 @@
 #!/bin/bash
 
-imageName=dnmp
+promptyn () {
+    while true; do
+        read -p "$1 " yn
+        case $yn in
+            [Yy]* ) return 0;;
+            [Nn]* ) return 1;;
+            * ) echo "Please enter Yes or No";;
+        esac
+    done
+}
 
-docker rmi ${imageName}
-docker system prune -af
-docker build -t ${imageName} . --no-cache
+resetVolumes () {
+    rm -rf ./volumes
+    mkdir -p ./volumes/settings
 
-mkdir -p ./volumes/settings
-cp ./builds/settings/config.json.example ./volumes/settings/config.json
+    cp ./builds/settings/config.json.example ./volumes/settings/config.json
 
-mkdir -p ./volumes/settings/templates/php
-cp ./builds/php/templates/php-cli.ini.example ./volumes/settings/templates/php/php-cli.ini
-cp ./builds/php/templates/php-fpm.ini.example ./volumes/settings/templates/php/php-fpm.ini
+    mkdir -p ./volumes/settings/templates/php
+    cp ./builds/php/templates/php-cli.ini.example ./volumes/settings/templates/php/php-cli.ini
+    cp ./builds/php/templates/php-fpm.ini.example ./volumes/settings/templates/php/php-fpm.ini
 
-mkdir -p  ./volumes/settings/templates/nginx
-cp ./builds/nginx/templates/default.conf.example ./volumes/settings/templates/nginx/default.conf
+    mkdir -p  ./volumes/settings/templates/nginx
+    cp ./builds/nginx/templates/default.conf.example ./volumes/settings/templates/nginx/default.conf
+}
+
+buildImage () {
+    docker rmi $1
+    docker system prune -af
+    docker build -t $1 . --no-cache
+}
+
+main () {
+    IMAGE_NAME=dnmp
+
+    if promptyn "Do you want to overwrite volumes folder? (y/n)"; then
+        resetVolumes
+    fi
+
+    buildImage ${IMAGE_NAME}
+}
+
+main
